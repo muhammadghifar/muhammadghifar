7770f60bbcb3aba9fefdf8404fe57db603877235
Muhammad Ghifar <muhammadghifarrijali@gmail.com>
2026-01-23 10:46:42 +0700

    fix: :ambulance: error build

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  // plugins: [vue()],
   test: {
     globals: true,
     environment: 'happy-dom',
